ec5eb682cdca99a6278a64e946d586590113bea3
oasdhjkv <140677136+oasdhjkv@users.noreply.github.com>
2023-07-27 02:51:40 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 #请修改为自己的UUID
-export UUID=ba3f6e6d-0648-4b39-b35d-36d2355e719
+export UUID=0bebe59f-18ab-4140-b706-54d2d536211f
 
 #请修改为自己设置的伪装站，不要带https://
-export ProxySite=www.holehike.com
+export ProxySite=end.gg
 
 #端口，如无特殊需求请勿更改,如果要改请一并修改dockerfile中的端口
 export Port=8080
